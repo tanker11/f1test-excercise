6c77f80ae42289a8512c28d73802ee772591b610
John Doe <johndoewebdev@example.com>
2024-11-22 19:26:49 +0100

    restructured the proof of concept folder

--- samples-trials/apitest1.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-import json
-
-response = urlopen('https://api.openf1.org/v1/meetings?year=2023')
-data = json.loads(response.read().decode('utf-8'))
-for meeting in data:
-    print(meeting['meeting_name'])
-
